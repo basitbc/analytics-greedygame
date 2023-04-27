7959a857fb29ad52930404badf645e7cfdc2d1f6
basitmarvelminds <basit.marvelminds@gmail.com>
2023-04-27 13:05:08 +0530

    added headers

--- src/modules/Analytics.jsx
+++ src/modules/Analytics.jsx
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
+const headers = {
+  'Content-Type': 'application/json',
+  'User-Agent': 'My App 1.0',
+};
+
 class Analytics {
   getAllReports(startDate, endDate) {
     return axios.get(
-      `http://go-dev.greedygame.com/v3/dummy/report?startDate=${startDate}&endDate=${endDate}`
+      `http://go-dev.greedygame.com/v3/dummy/report?startDate=${startDate}&endDate=${endDate}`,
+      { headers }
     );
   }
   getAllApps() {
-    return axios.get(`http://go-dev.greedygame.com/v3/dummy/apps`);
+    return axios.get(`http://go-dev.greedygame.com/v3/dummy/apps`, { headers });
   }
 }
 
